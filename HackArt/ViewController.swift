f4503e9f0bbe8803ab9ece03c12cb1190b268e0c
Paweł Czerwiński <22awik@gmail.com>
2018-05-26 10:19:42 +0200

    Initial Commit

---
+++ HackArt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackArt
+//
+//  Created by Paweł Czerwiński on 26/05/2018.
+//  Copyright © 2018 BoroCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
