42db7d77d445d1aec8a808c4faab8f429ce862ed
Mateusz Orzoł <mateusz.orzol@proexe.pl>
2018-05-27 15:04:41 +0200

    Add face recognition XDDD

--- HackArt/Models/OriginPainting.swift
+++ HackArt/Models/OriginPainting.swift
@@ -32,46 +32,38 @@ struct OriginPainting {
             painting: Painting(name: nil, image: #imageLiteral(resourceName: "sredniowiecze")),
             title: "Wit",
             author: "Wit",
-            tags: [Tag.smutek],
+            tags: [Tag.snieg],
             identifier: "sredniowiecze"
           ),
           OriginPainting(
             painting: Painting(name: "Witkacy", image: #imageLiteral(resourceName: "Bitmap")),
-            title: "Wit",
-            author: "Wit",
+            title: "Kompozycja fantastyczna",
+            author: "Witkiewicz, Stanisław Ignacy",
             tags: [Tag.slonko],
             identifier: "witkacy"
           ),
           OriginPainting(
-            painting: Painting(name: nil, image: #imageLiteral(resourceName: "radość")),
-            title: "Wit",
-            author: "Wit",
+            painting: Painting(name: "Swinka", image: #imageLiteral(resourceName: "radość")),
+            title: "Zabawka na kiju \"Świnka\"",
+            author: "Kurzątkowski, Jan",
             tags: [Tag.radosc],
             identifier: "swiniak"
           ),
           OriginPainting(
             painting: Painting(name: "przepiorki", image: #imageLiteral(resourceName: "przepiorki")),
-            title: "Wit",
-            author: "Wit",
+            title: "Kuropatwy na śniegu",
+            author: "Chełmoński, Józef",
             tags: [Tag.przepiorka],
             identifier: "przepiorki"
           ),
           OriginPainting(
-            painting: Painting(name: nil, image: #imageLiteral(resourceName: "smutek")),
-            title: "Wit",
-            author: "Wit",
+            painting: Painting(name: "Roztrzelanie", image: #imageLiteral(resourceName: "smutek")),
+            title: "Roztrzelanie",
+            author: "Wróblewski, Andrzej",
             tags: [Tag.smutek],
             identifier: "smutek"
-          ),
-          OriginPainting(
-            painting: Painting(name: nil, image: #imageLiteral(resourceName: "spokój")),
-            title: "Wit",
-            author: "Wit",
-            tags: [Tag.spokoj],
-            identifier: "spokój"
-          ),
+          )
         ]
-//        return [OriginPainting(image: UIImage(named: "0017.jpg")!, description: "Przepiórki tatatat", tags: [Tag.przepiorka, Tag.spokoj], identifier: "przepiorki")]
     }
     
 }
